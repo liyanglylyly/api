9ad5622a88416a25a94b75b73a5d6b470f0b8001
liyang <liyang@huocheetc.com>
2024-08-13 20:26:35 +0800

    切换mysql

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,23 @@
 import { resolve } from 'path';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// export const database = (): TypeOrmModuleOptions => ({
+//   type: 'better-sqlite3',
+//   database: resolve(__dirname, '../../database.db'),
+//   synchronize: true,
+//   autoLoadEntities: true,
+// });
+
 export const database = (): TypeOrmModuleOptions => ({
-  type: 'better-sqlite3',
-  database: resolve(__dirname, '../../database.db'),
+  type: 'mysql',
+  host: '124.221.1.101',
+  port: 3306,
+  username: 'root',
+  password: 'admin123',
+  database: 'mall',
   synchronize: true,
+  logging: true,
   autoLoadEntities: true,
+  poolSize: 10,
+  connectorPackage: 'mysql2',
 });
